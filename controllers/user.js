582f2b6c52ef688de35d4d7291f3bad2ea7013b2
Rushabh2397 <rushabhsancheti2397@gmail.com>
2021-10-02 19:36:04 +0530

    new build and fix login issue

--- controllers/user.js
+++ controllers/user.js
@@ -28,6 +28,7 @@ module.exports = {
                 })
             },
             (body, nextCall) => {
+                req.body.email = req.body.email.toLowerCase();
                 let user = new User(body)
                 user.save((err, data) => {
                     if (err) {
@@ -53,6 +54,7 @@ module.exports = {
     login: (req, res) => {
         async.waterfall([
             (nextCall) => {
+                req.body.email = req.body.email.totoLowerCase();
                 const errors = validationResult(req);
                 if (!errors.isEmpty()) {
                     return nextCall({ message: errors.errors[0].msg });
